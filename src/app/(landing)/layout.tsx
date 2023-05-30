a7cb8be04cabff9112525a619ae991507c11df5b
Lucca Paradeda <luccatisserparadeda@gmail.com>
2023-05-30 18:00:36 -0300

    feat: metadata head

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -1,11 +1,39 @@
 import './globals.css'
 import { Sora } from 'next/font/google'
+import { Metadata } from 'next'
 
 const sora = Sora({ subsets: ['latin'] })
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Lucca Paradeda',
   description: 'Site pessoal de Lucca Paradeda',
+  icons: [
+    {
+      href: '/lucca.jpg',
+      url: '/lucca.jpg',
+      sizes: '80x160',
+      type: 'image/jpg',
+      rel: 'icon',
+    },
+  ],
+  creator: 'Lucca Paradeda',
+  authors: [{ name: 'Lucca Paradeda' }],
+  keywords: [
+    'Lucca Paradeda',
+    'Lucca',
+    'Paradeda',
+    'Programming',
+    'Programação',
+    'Desenvolvimento',
+    'Development',
+    'Web',
+    'Web Development',
+    'Desenvolvimento Web',
+    'Frontend',
+    'Frontend Development',
+  ],
+
+
 }
 
 export default function RootLayout({
